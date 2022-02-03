0dc8bf530f17207b594193095268a04f8fa404cc
MuXiu1997 <muxiu1997@gmail.com>
2022-02-03 16:55:41 +0800

    test operator YearMonth

--- src/main/kotlin/org/example/test-operator.kt
+++ src/main/kotlin/org/example/test-operator.kt
@@ -1,7 +1,12 @@
 package org.example
 
+import org.apache.commons.lang3.time.DateFormatUtils
+import org.apache.commons.lang3.time.DateUtils
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.util.Date
 
 
 fun toBigDecimal(any: Any?): BigDecimal {
@@ -57,7 +62,83 @@ fun <T> Sequence<T>?.safeSumOf(selector: (T) -> Any?): BigDecimal {
     return this?.map(selector)?.fold(BigDecimal.ZERO) { a, b -> a safeAdd b } ?: BigDecimal.ZERO
 }
 
+fun fromDate(date: Date): YearMonth {
+    return YearMonth.parse(DateFormatUtils.format(date, "yyyy-MM"))
+}
+
+fun fromLocalDate(localDate: LocalDate): YearMonth {
+    return YearMonth.of(localDate.year, localDate.month)
+}
+
+fun fromLocalDateTime(localDateTime: LocalDateTime): YearMonth {
+    return YearMonth.of(localDateTime.year, localDateTime.month)
+}
+
+fun YearMonth.toYear(): Year {
+    return Year.of(this.year)
+}
+
+fun YearMonth.toDate(dayOfMonth: Int = 1): Date {
+    return DateUtils.parseDate(this.toLocalDate(dayOfMonth).format(DateTimeFormatter.ISO_LOCAL_DATE), "yyyy-MM-dd")
+}
+
+fun YearMonth.toLocalDate(dayOfMonth: Int = 1): LocalDate {
+    return LocalDate.of(this.year, this.month, dayOfMonth)
+}
+
+fun YearMonth.toLocalDateTime(dayOfMonth: Int = 1, hour: Int = 0, minute: Int = 0, second: Int = 0): LocalDateTime {
+    return LocalDateTime.of(this.year, this.month, dayOfMonth, hour, minute, second)
+}
+
+fun YearMonth.prevMonth(): YearMonth {
+    return this.minusMonths(1)
+}
+
+fun YearMonth.nextMonth(): YearMonth {
+    return this.plusMonths(1)
+}
+
+fun YearMonth.prevYear(): YearMonth {
+    return this.minusYears(1)
+}
+
+fun YearMonth.nextYear(): YearMonth {
+    return this.plusYears(1)
+}
+
+fun YearMonth.withYear(year: Year): YearMonth {
+    return YearMonth.of(year.value, this.monthValue)
+}
+
+fun YearMonth.withMonth(month: Month): YearMonth {
+    return YearMonth.of(this.year, month)
+}
+
+class YearMonthRange(override val start: YearMonth, override val endInclusive: YearMonth) : Iterable<YearMonth>,
+    ClosedRange<YearMonth> {
+    override fun iterator(): Iterator<YearMonth> {
+        return object : Iterator<YearMonth> {
+            var current = start
+            override fun hasNext(): Boolean {
+                return current <= endInclusive
+            }
+
+            override fun next(): YearMonth {
+                return current.apply { current = nextMonth() }
+            }
+        }
+    }
+}
+
+operator fun YearMonth.rangeTo(other: YearMonth): YearMonthRange {
+    return YearMonthRange(this, other)
+}
+
 fun main() {
     println((1 safeAdd 2))
     println(listOf(1,2,3).safeSum())
+
+    (fromLocalDate(LocalDate.ofYearDay(2021,1))..YearMonth.of(2021,12)).forEach {
+        println(it)
+    }
 }
